da335e2e0175c817a9d1c89225391f221e273a9b
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-03-14 12:16:33 -0400

    API calls for existing content. A form to capture new student info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import StudentInputForm from "./pages/StudentInputForm";
+// import config from "./config.js";
 function App() {
+    const [students, setStudents] = useState([]);
+    const getStudents = async () => {
+      const response = await fetch(
+          'https://txrothumatsyjpckbgrl.supabase.co/rest/v1/students?',  {
+            headers: {
+              apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4cm90aHVtYXRzeWpwY2tiZ3JsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg3MjgyNDksImV4cCI6MTk5NDMwNDI0OX0.dVF59XvFesnIvalXRzCWxRMgsYiEoHEUshpB1TkgDbg",
+              Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4cm90aHVtYXRzeWpwY2tiZ3JsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg3MjgyNDksImV4cCI6MTk5NDMwNDI0OX0.dVF59XvFesnIvalXRzCWxRMgsYiEoHEUshpB1TkgDbg",
+
+            // "Authorization": `Bearer ${config.API_KEY}`,
+            // "Content-Type": "application/json"
+            },
+          });
+      const students = await response.json();
+      console.log(students)
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <StudentInputForm/>
+        <button onClick={() => getStudents()} className={'bg-blue-300'}>get students</button>
+
     </div>
   );
 }
